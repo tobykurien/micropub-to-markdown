f6880c6d3d56ae615170565fac8f500269573f7b
Toby Kurien <toby@tobykurien.com>
2020-12-30 12:01:12 +0200

    add chown

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,3 +4,6 @@ sudo apt install imagemagick inotify-tools
 # create the POSTS_ROOT directory structure where posts are saved
 mkdir -p /home/user/posts/microblog
 mkdir -p /home/user/posts/images/microblog
+
+# set owner so that the web server can write to these directories
+chown -R www-data:www-data /home/user/posts
