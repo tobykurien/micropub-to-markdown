adbce201947b734b974c068c9417c98c46184705
Toby Kurien <toby@tobykurien.com>
2020-12-30 12:02:03 +0200

    extract posts_root

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+POSTS_ROOT=/home/user/posts
+
 sudo apt install imagemagick inotify-tools
 
 # create the POSTS_ROOT directory structure where posts are saved
-mkdir -p /home/user/posts/microblog
-mkdir -p /home/user/posts/images/microblog
+mkdir -p $POSTS_ROOT/microblog
+mkdir -p $POSTS_ROOT/images/microblog
 
 # set owner so that the web server can write to these directories
-chown -R www-data:www-data /home/user/posts
+chown -R www-data:www-data $POSTS_ROOT
